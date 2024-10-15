5eac738a5d4c8ff85c26d17ceeb2b5509dc983af
Jaehong-Daniel-Kim <jaehong.daniel.kim@gmail.com>
2024-10-16 08:19:04 +0900

    resend code button and timer

--- frontend/src/components/signUpModal/emailVerificationModal.tsx
+++ frontend/src/components/signUpModal/emailVerificationModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3706d6cb9eab63e8fe08d15c6709044dab1ccfe01c239fe33e65bfc6e7b13c0a
-size 6928
+oid sha256:d053d42ec955584662eb50f472c300b8089b5bc1385d92a79bbd3dc7af2ee8a0
+size 7612
