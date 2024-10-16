f24f23270873c902bc4139026a621a97cd75122f
Jaehong-Daniel-Kim <jaehong.daniel.kim@gmail.com>
2024-10-16 11:10:21 +0900

    emailValidationModal type annotation

--- frontend/src/components/signUpModal/emailVerificationModal.tsx
+++ frontend/src/components/signUpModal/emailVerificationModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d053d42ec955584662eb50f472c300b8089b5bc1385d92a79bbd3dc7af2ee8a0
-size 7612
+oid sha256:e63c2cd004725cdf67739434ca3af2c887460a4d196f40e7f68947e6c45a4d66
+size 7684
